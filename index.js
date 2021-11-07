9e84ea783041ed87972d1059c62de768a97cc859
Martha Solomon <marthsolomon@gmail.com>
2021-11-07 09:26:38 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) 
+  return 'This one is on me!';
+  else if (someValue >= 2000 && someValue < 2500) 
+  return 'I will gladly take your thirty bucks.';
+  else if (someValue > 2500) 
+  return 'No can do.';
+  
 }
+function ternaryCheckCity(someValue){
+  return someValue === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  }
+  
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someValue){
+  switch(someValue){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
